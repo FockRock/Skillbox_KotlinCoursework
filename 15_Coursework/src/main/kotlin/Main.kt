7c1c1707152c38c878d53691557ec7ee4a71e318
FockRock <smirnov_nikita@mail.ru>
2023-12-27 22:20:42 +0300

    Functions created

--- 15_Coursework/src/main/kotlin/Main.kt
+++ 15_Coursework/src/main/kotlin/Main.kt
@@ -1,43 +1,103 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
 
-var smallTruckCount = 0
-var mediumTruckCount = 0
-var bigTruckCount = 0
-
 fun main() {
+    val channel: Channel<Goods>
     runBlocking {
         val cargoLoadPoint1 = launch {
-            println("Началась погрузка...")
+            println("Truck is coming...")
             Depot.flow.collect {
-                println(it.name)
-                it.addGoods(apple)
-                it.addGoods(table)
+                if (it.goodsList.isNotEmpty()) it.unloading()
+                else {
+                    val i = Random.nextInt(0, 4)
+                    while (it.weight < it.maxWeight) {
+                        if (i == 0) it.addGoods(edibleProducts[Random.nextInt(0, 4)])
+                        else it.addGoods(notEdibleProducts[Random.nextInt(0, 11)])
+                    }
+                }
                 cancel()
             }
         }
     }
 }
 
+var smallTruckCount = 0
+var mediumTruckCount = 0
+var bigTruckCount = 0
+
 object Depot {
     val flow = flow {
         while (currentCoroutineContext().isActive) {
             val b: Truck
-            when (Random.nextInt(1, 4)) {
+            when (Random.nextInt(1, 16)) {
                 1 -> {
                     b = SmallTruck()
+                    b.isEmpty = true
                     smallTruckCount++
+                    println("${b.name} is empty and ready to load!")
                 }
                 2 -> {
                     b = MediumTruck()
+                    b.isEmpty = true
                     mediumTruckCount++
+                    println("${b.name} is empty and ready to load!")
+                }
+                3 -> {
+                    b = BigTruck()
+                    b.isEmpty = true
+                    bigTruckCount++
+                    println("${b.name} is empty and ready to load!")
+                }
+                4, 5, 6 -> {
+                    b = SmallTruck()
+                    smallTruckCount++
+                    while (b.weight < b.maxWeight) {
+                        b.loading(notEdibleProducts[Random.nextInt(0, 11)])
+                    }
+                    println("${b.name} is ready to unload!")
+                }
+                7, 8, 9 -> {
+                    b = MediumTruck()
+                    mediumTruckCount++
+                    while (b.weight < b.maxWeight) {
+                        b.loading(notEdibleProducts[Random.nextInt(0, 11)])
+                    }
+                    println("${b.name} is ready to unload!")
+                }
+                10, 11, 12 -> {
+                    b = BigTruck()
+                    bigTruckCount++
+                    while (b.weight < b.maxWeight) {
+                        b.loading(notEdibleProducts[Random.nextInt(0, 11)])
+                    }
+                    println("${b.name} is ready to unload!")
+                }
+                13 -> {
+                    b = SmallTruck()
+                    smallTruckCount++
+                    while (b.weight < b.maxWeight) {
+                        b.loading(edibleProducts[Random.nextInt(0, 4)])
+                    }
+                    println("${b.name} is ready to unload!")
+                }
+                14 -> {
+                    b = MediumTruck()
+                    mediumTruckCount++
+                    while (b.weight < b.maxWeight) {
+                        b.loading(edibleProducts[Random.nextInt(0, 4)])
+                    }
+                    println("${b.name} is ready to unload!")
                 }
                 else -> {
                     b = BigTruck()
-//                    b.goodsList.add(notEdibleProducts[Random.nextInt(0,10)])
                     bigTruckCount++
+                    while (b.weight < b.maxWeight) {
+                        b.loading(edibleProducts[Random.nextInt(0, 4)])
+                    }
+                    println("${b.name} is ready to unload!")
                 }
             }
             delay(1000)
@@ -46,19 +106,39 @@ object Depot {
     }
 }
 
-abstract class Truck(var maxWeight: Int, var name: String, var isEmpty: Boolean = false, var weight: Int = 0){
+object Warehouse {
+    val notEatableGoods = mutableListOf<Goods>()
+    val eatableGoods = mutableListOf<Goods>()
+}
+
+abstract class Truck(var maxWeight: Int, var name: String, var isEmpty: Boolean = false, var weight: Int = 0) {
 
     var goodsList = mutableListOf<Goods>()
 
-    fun addGoods (g: Goods) {
-        goodsList.add(g)
-        println("${g.name} added to $name!")
-        weight += g.weight
-        println("Total weight $weight")
+    suspend fun addGoods(g: Goods) {
+        if (g.weight <= maxWeight - weight) {
+            goodsList.add(g)
+            delay(g.time.toLong())
+            println("${g.name} added to $name!")
+            weight += g.weight
+            println("Total weight $weight")
+        } else println("${g.name} cannot fit!")
     }
-    fun unloading () {
+
+    fun loading(g: Goods) {
+        if (g.weight <= maxWeight - weight) {
+            goodsList.add(g)
+            weight += g.weight
+
+        }
+    }
+
+    suspend fun unloading() {
         while (goodsList.isNotEmpty()) {
-            println("${goodsList[0]} unloading!")
+            println("${goodsList[0].name} unloading from $name!")
+            delay(goodsList[0].time.toLong())
+            if (goodsList[0].isEatable) Warehouse.eatableGoods.add(goodsList[0])
+            else Warehouse.notEatableGoods.add(goodsList[0])
             goodsList.removeAt(0)
         }
         println("$name is Empty!")
@@ -69,23 +149,24 @@ class SmallTruck : Truck(1500, "SmallTruck ${smallTruckCount + 1}")
 class MediumTruck : Truck(2500, "MediumTruck ${mediumTruckCount + 1}")
 class BigTruck : Truck(3500, "BigTruck ${bigTruckCount + 1}")
 
-class Goods(var weight: Int, var time: Int, val name: String)
+class Goods(var weight: Int, var time: Int, val name: String,val isEatable: Boolean = false)
 
-val apple = Goods(100, 1500, "Apples")
-val milk = Goods(200, 3000,"Milk")
-val bread = Goods(100, 500,"Bread")
-val meat = Goods(200, 1000,"Meat")
-val table = Goods(300, 3000,"Table")
-val chair = Goods(200, 3000,"Chair")
-val computer = Goods(100, 3000,"Computer")
-val wardrobe = Goods(600, 5000,"Wardrobe")
-val door = Goods(400, 4000,"Door")
-val lamp = Goods(300, 3000,"Lamp")
-val laptop = Goods(100, 3000,"Laptop")
-val sink = Goods(100, 3000,"Sink")
-val armchair = Goods(100, 3000,"Armchair")
-val window = Goods(100, 3000,"Window")
-val tv = Goods(100, 3000,"TV")
+val apple = Goods(100, 15, "Apples", true)
+val milk = Goods(200, 30, "Milk", true)
+val bread = Goods(100, 5, "Bread", true)
+val meat = Goods(200, 10, "Meat", true)
+val table = Goods(300, 30, "Table")
+val chair = Goods(200, 30, "Chair")
+val computer = Goods(200, 30, "Computer")
+val wardrobe = Goods(600, 50, "Wardrobe")
+val door = Goods(400, 40, "Door")
+val lamp = Goods(300, 30, "Lamp")
+val laptop = Goods(100, 30, "Laptop")
+val sink = Goods(200, 30, "Sink")
+val armchair = Goods(100, 30, "Armchair")
+val window = Goods(300, 30, "Window")
+val tv = Goods(200, 30, "TV")
 
-val edibleProducts = mutableListOf(apple,milk,bread,meat)
-val notEdibleProducts = mutableListOf(table,chair,computer,wardrobe,door,lamp,laptop,sink,armchair,window,tv)
+val edibleProducts = mutableListOf(apple, milk, bread, meat)
+val notEdibleProducts =
+    mutableListOf(table, chair, computer, wardrobe, door, lamp, laptop, sink, armchair, window, tv)
