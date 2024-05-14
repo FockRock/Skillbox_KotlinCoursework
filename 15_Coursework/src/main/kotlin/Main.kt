0c799ce182641260b0662994e7bfccd5c7b8f4c8
FockRock <smirnov_nikita@mail.ru>
2024-05-14 22:15:08 +0300

    job2 added

--- 15_Coursework/src/main/kotlin/Main.kt
+++ 15_Coursework/src/main/kotlin/Main.kt
@@ -9,7 +9,42 @@ fun main() {
         val scope = CoroutineScope(this.coroutineContext)
         val traffic = mutableListOf<Truck>()
 
-        scope.launch {
+        val job1 = scope.launch {
+
+            Depot.flow.collect { truck ->
+                val a = Random.nextInt(2)
+                if (!truck.isEmpty) truck.unloading()
+                else if (a == 0) {
+                    if (Warehouse.notEatableGoods.isEmpty()) {
+                        println("${truck.name} go to the traffic!")
+                        traffic.add(truck)
+                    } else {
+                        while (truck.weight < truck.maxWeight && Warehouse.notEatableGoods.isNotEmpty() && Warehouse.notEatableGoods[0].weight < (truck.maxWeight - truck.weight))
+                            truck.addGoods(Warehouse.notEatableGoods[0], false)
+                    }
+                } else if (a == 1) {
+                    if (Warehouse.eatableGoods.isEmpty()) {
+                        println("${truck.name} go to the traffic!")
+                        traffic.add(truck)
+                    } else {
+                        while (truck.weight < truck.maxWeight && Warehouse.eatableGoods.isNotEmpty() && Warehouse.eatableGoods[0].weight < (truck.maxWeight - truck.weight))
+                            truck.addGoods(Warehouse.eatableGoods[0], true)
+                    }
+                }
+                if (smallTruckCount == 3 || mediumTruckCount == 3 || bigTruckCount == 3) {
+                    println("Не съедобные товары на складе:")
+                    Warehouse.notEatableGoods.forEach { println(it.goodName) }
+                    println("Съедобные товары на складе:")
+                    Warehouse.eatableGoods.forEach { println(it.goodName) }
+                    println("Машины в очереди:")
+                    traffic.forEach { println(it.name) }
+                    cancel()
+                }
+                delay(1000)
+            }
+        }
+
+        val job2 = scope.launch {
 
             Depot.flow.collect { truck ->
                 val a = Random.nextInt(2)
