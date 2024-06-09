6e2bb29508a2d571d79b7019d3ff563eb5fc41cc
FockRock <smirnov_nikita@mail.ru>
2024-06-09 15:18:15 +0300

    unloading fun created

--- 15_Coursework/src/main/kotlin/Main.kt
+++ 15_Coursework/src/main/kotlin/Main.kt
@@ -6,69 +6,46 @@ import kotlin.random.Random
 
 fun main() {
     runBlocking {
-        val channel = Channel<Truck>()
-        val traffic = mutableListOf<Truck>()
         val scope = CoroutineScope(this.coroutineContext)
         val loadTraffic = mutableListOf<Truck>()
 
-//        launch {
-//            Depot.flow.collect {
-//                traffic.add(it)
-//                if(it.isEmpty)
-//                    channel.send(it)
-//                if (traffic.size == 6) {
-//                    cancel()
-//                    channel.close()
-//                }
-//            }
-//        }
-//        for (a in channel)
-//            loadTraffic.add(a)
-//        println("List of trucks in line:")
-//        loadTraffic.forEach {
-//            println(it.name)
-//        }
-//        println("Done!")
-
         //Сделать три корутины с выгрузкой, и одну с загрузкой
-
 
         val job1 = scope.launch {
 
             Depot.flow.collect { truck ->
-                val a = Random.nextInt(2)
-                if (!truck.isEmpty) truck.unloading()
-                else if (a == 0) {
-                    if (Warehouse.notEatableGoods.isEmpty()) {
-                        println("${truck.name} go to the traffic!")
-                        traffic.add(truck)
-                        channel.send(truck)
-                    } else {
-                        while (truck.weight < truck.maxWeight && Warehouse.notEatableGoods.isNotEmpty() && Warehouse.notEatableGoods[0].weight < (truck.maxWeight - truck.weight))
-                            truck.addGoods(Warehouse.notEatableGoods[0], false)
-                    }
-                } else if (a == 1) {
-                    if (Warehouse.eatableGoods.isEmpty()) {
-                        println("${truck.name} go to the traffic!")
-                        traffic.add(truck)
-                        channel.send(truck)
-                    } else {
-                        while (truck.weight < truck.maxWeight && Warehouse.eatableGoods.isNotEmpty() && Warehouse.eatableGoods[0].weight < (truck.maxWeight - truck.weight))
-                            truck.addGoods(Warehouse.eatableGoods[0], true)
-                    }
-                }
-//                if (smallTruckCount == 3 || mediumTruckCount == 3 || bigTruckCount == 3) {
-//                    println("Не съедобные товары на складе:")
-//                    Warehouse.notEatableGoods.forEach { println(it.goodName) }
-//                    println("Съедобные товары на складе:")
-//                    Warehouse.eatableGoods.forEach { println(it.goodName) }
-//                    channel.close()
-//                    cancel()
-//                }
+                if (!truck.isEmpty) truck.unloading() else loadFun(truck)
                 delay(1000)
+
+                if (smallTruckCount == 3 || mediumTruckCount == 3 || bigTruckCount == 3) {
+                    println("Не съедобные товары на складе:")
+                    Warehouse.notEatableGoods.forEach { println(it.goodName) }
+                    println("Съедобные товары на складе:")
+                    Warehouse.eatableGoods.forEach { println(it.goodName) }
+                    channel.close()
+                    cancel()
+                }
             }
         }
-        val job2 = launch {
+
+        val job2 = scope.launch {
+
+            Depot.flow.collect { truck ->
+                if (!truck.isEmpty) truck.unloading() else loadFun(truck)
+                delay(1000)
+
+                if (smallTruckCount == 3 || mediumTruckCount == 3 || bigTruckCount == 3) {
+                    println("Не съедобные товары на складе:")
+                    Warehouse.notEatableGoods.forEach { println(it.goodName) }
+                    println("Съедобные товары на складе:")
+                    Warehouse.eatableGoods.forEach { println(it.goodName) }
+                    channel.close()
+                    cancel()
+                }
+            }
+        }
+
+        val job3 = launch {
             for (a in channel)
                 loadTraffic.add(a)
             println("List of trucks in line:")
@@ -77,41 +54,31 @@ fun main() {
             }
             println("Done!")
         }
+    }
+}
 
-        val job3 = scope.launch {
 
-            Depot.flow.collect { truck ->
-                val a = Random.nextInt(2)
-                if (!truck.isEmpty) truck.unloading()
-                else if (a == 0) {
-                    if (Warehouse.notEatableGoods.isEmpty()) {
-                        println("${truck.name} go to the traffic!")
-                        traffic.add(truck)
-                        channel.send(truck)
-                    } else {
-                        while (truck.weight < truck.maxWeight && Warehouse.notEatableGoods.isNotEmpty() && Warehouse.notEatableGoods[0].weight < (truck.maxWeight - truck.weight))
-                            truck.addGoods(Warehouse.notEatableGoods[0], false)
-                    }
-                } else if (a == 1) {
-                    if (Warehouse.eatableGoods.isEmpty()) {
-                        println("${truck.name} go to the traffic!")
-                        traffic.add(truck)
-                        channel.send(truck)
-                    } else {
-                        while (truck.weight < truck.maxWeight && Warehouse.eatableGoods.isNotEmpty() && Warehouse.eatableGoods[0].weight < (truck.maxWeight - truck.weight))
-                            truck.addGoods(Warehouse.eatableGoods[0], true)
-                    }
-                }
-
-                delay(1000)
-            }
+val channel = Channel<Truck>()
+val traffic = mutableListOf<Truck>()
+suspend fun loadFun(t: Truck){
+    val a = Random.nextInt(2)
+    if (a == 0) {
+        if (Warehouse.notEatableGoods.isEmpty()) {
+            println("${t.name} go to the traffic!")
+            traffic.add(t)
+            channel.send(t)
+        } else {
+            while (t.weight < t.maxWeight && Warehouse.notEatableGoods.isNotEmpty() && Warehouse.notEatableGoods[0].weight < (t.maxWeight - t.weight))
+                t.addGoods(Warehouse.notEatableGoods[0], false)
         }
-//        for (a in channel)
-//            loadTraffic.add(a)
-//        println("List of trucks in line:")
-//        loadTraffic.forEach {
-//            println(it.name)
-//        }
-//        println("Done!")
+    } else if (a == 1) {
+        if (Warehouse.eatableGoods.isEmpty()) {
+            println("${t.name} go to the traffic!")
+            traffic.add(t)
+            channel.send(t)
+        } else {
+            while (t.weight < t.maxWeight && Warehouse.eatableGoods.isNotEmpty() && Warehouse.eatableGoods[0].weight < (t.maxWeight - t.weight))
+                t.addGoods(Warehouse.eatableGoods[0], true)
+        }
     }
 }
